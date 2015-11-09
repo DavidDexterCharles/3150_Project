8bf6958c0d6fbd8b541058a2da02556114315cf9
David Charles <david.dexter.charles@gmail.com>
2015-11-09 11:19:43 -0400

    simple update

--- TCP_PYTHON/QT_App/client_gui.py
+++ TCP_PYTHON/QT_App/client_gui.py
@@ -1,28 +1,84 @@
 '''
 Created on Sep 16, 2015
-
+#https://pythonspot.com/pyqt4/
 @author: David
 '''
 
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+
 import sys
-from PyQt4.QtGui import *
+from PyQt4.QtCore import pyqtSlot # needed for @pyqtSlot() to work
+from PyQt4.QtGui import * # imports all the gui components
  
-# Create an PyQT4 application object.
-a = QApplication(sys.argv)       
  
-# The QWidget widget is the base class of all user interface objects in PyQt4.
-w = QWidget()
+# create our window
+app = QApplication(sys.argv) # Create an PyQT4 application object.      
+w = QWidget()# The QWidget widget is the base class of all user interface objects in PyQt4.
+
+#w=QMainWindow()# The QWidget widget is the base class of all user interface objects in PyQt4.
+
+w.resize(600, 600)# Set window size. 
+w.setWindowTitle("Hello World! Time for QT Training") # Set window title  
+
+
+# Create main menu
+# mainMenu = w.menuBar()
+# fileMenu = mainMenu.addMenu('&File')
+
+# Add exit button
+# exitButton = QAction(QIcon('exit24.png'), 'Exit', w)
+# exitButton.setShortcut('Ctrl+Q')
+# exitButton.setStatusTip('Exit application')
+# exitButton.triggered.connect(w.close)
+# fileMenu.addAction(exitButton)
+
+
  
-# Set window size. 
-w.resize(400, 400)
+# Add a button exit button
+btnexit = QPushButton('Exit', w)   # creates a button in the window
+btnexit.setToolTip('Click to close window!')
+btnexit.clicked.connect(exit)
+btnexit.resize(btnexit.sizeHint())
+btnexit.move(100, 80) # move(x, y) if x increase you go to the right if y increase you go down
+
+
+# Add another button 
+btn1 = QPushButton('My output show when clicked Shadae', w)
+btn1.move(200, 80)
+btn2 = QPushButton('My output show when pressed', w)
+btn2.move(300, 150)
+btn3 = QPushButton(' My output show on release', w)
+btn3.move(400, 200)
+
+
+# Create the actions 
+@pyqtSlot()
+def on_click():
+    result = QMessageBox.question(w, 'Message', "Do you like Python?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No) # Show a message box, not the site have different types of message box
+    if result == QMessageBox.Yes:
+        print 'Yes. was clicked'
+    else:
+        print 'No.  was clicked' 
+
+    
  
-# Set window title  
-w.setWindowTitle("Hello World!") 
+@pyqtSlot()
+def on_press():
+    print('pressed')
+ 
+@pyqtSlot()
+def on_release():
+    print('released')
+ 
+# connect the signals to the slots
+btn1.clicked.connect(on_click)
+btn2.pressed.connect(on_press)
+btn3.released.connect(on_release)
+
  
 # Show window
 w.show() 
  
-sys.exit(a.exec_())
+sys.exit(app.exec_())
--- TCP_PYTHON/QT_Appv1/client.py
+++ TCP_PYTHON/QT_Appv1/client.py
@@ -11,7 +11,7 @@ import socket
 TCP_IP = socket.gethostbyname("localhost")
 TCP_PORT = 4486
 BUFFER_SIZE = 1024
-MESSAGE =raw_input('Enter your message: ') #not raw_input have to be used in python versions lower than 3
+MESSAGE =raw_input('Enter your message: ') #note raw_input have to be used in python versions lower than 3
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
